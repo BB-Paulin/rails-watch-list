6a502656b36f2073f58ebe35f90c58e99c225ea3
Paulin Bougault-Bourges <paulin.bougault@yahoo.fr>
2023-02-23 15:17:28 +0100

    Prepare rails app with external specs

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,20 @@
 class BookmarksController < ApplicationController
+
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmarks_params)
+    if @bookmark.save!
+      redirect_to bookmarks_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  #resources :movie
-  #resources :bookmark
-  resources :lists
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks
+
   #end
   #ressources :list
   # Defines the root path route ("/")
